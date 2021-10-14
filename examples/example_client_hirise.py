81f1a1fc9d7275f44802e336f5e087874475519e
Evin Dunn <evindunn14@gmail.com>
2021-10-13 22:20:16 -0700

    HiRISE example produces 'true' color image

--- examples/example_client_hirise.py
+++ examples/example_client_hirise.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-import concurrent.futures
-import logging
 from concurrent.futures import ThreadPoolExecutor
 from sys import path as sys_path
 from os.path import dirname, basename, join as path_join, exists as path_exists, realpath, splitext
@@ -10,13 +8,13 @@ from tempfile import TemporaryDirectory
 from urllib.parse import urlparse
 from re import sub as re_sub
 from os import urandom
+from uuid import uuid4
 
 # Thanks to
 # https://repository.si.edu/bitstream/handle/10088/19366/nasm_201048.pdf?sequence=1&isAllowed=y
 # https://github.com/USGS-Astrogeology/ISIS3/issues/3257#issuecomment-518856485
-
+# https://www.lpi.usra.edu/meetings/lpsc2007/pdf/1779.pdf
 # for this
-from uuid import uuid4
 
 pkg_dir = dirname(dirname(realpath(__file__)))
 sys_path.insert(0, pkg_dir)
@@ -60,6 +58,7 @@ class HiRISEDetectorProcessor:
     def process(self):
         self._download_inputs()
         self._convert_to_cubes()
+        self._normalize_channels()
         self._stitch_channels()
 
     def _download_inputs(self):
@@ -91,6 +90,23 @@ class HiRISEDetectorProcessor:
         self.channel0 = cube_files[0].result()
         self.channel1 = cube_files[1].result()
 
+    def _normalize_channels(self):
+        normed_files = list()
+        with ThreadPoolExecutor() as pool:
+            for channel in [self.channel0, self.channel1]:
+                t = pool.submit(
+                    HiRISEDetectorProcessor._normalize_channel,
+                    self._isis_client,
+                    channel
+                )
+                normed_files.append(t)
+
+        self._isis_client.rm(self.channel0)
+        self._isis_client.rm(self.channel1)
+
+        self.channel0 = normed_files[0].result()
+        self.channel1 = normed_files[1].result()
+
     def _stitch_channels(self):
         out_file = "{}_{}.cub".format(self.image_id, self.detector)
         histitch = self._isis_client.command("histitch")
@@ -116,7 +132,7 @@ class HiRISEDetectorProcessor:
 
         spiceinit = client.command("spiceinit")
         spiceinit.add_arg("from", cub_file)
-        # spiceinit.add_arg("web", "true")
+        spiceinit.add_arg("web", "true")
         spiceinit.send()
 
         cal_file = "{}.cal.cub".format(hirise_basename)
@@ -128,6 +144,17 @@ class HiRISEDetectorProcessor:
         client.rm(cub_file)
 
         return cal_file
+
+    @staticmethod
+    def _normalize_channel(isis_client: ISISClient, channel_file: str):
+        norm_file = "{}.norm.cub".format(strip_ext(channel_file))
+
+        cubenorm = isis_client.command("cubenorm")
+        cubenorm.add_arg("from", channel_file)
+        cubenorm.add_arg("to", norm_file)
+        cubenorm.send()
+
+        return norm_file
 
     @staticmethod
     def get_img_url(pdsimage_path, pdsimage_id, detector, channel):
@@ -149,6 +176,7 @@ class HiRISETrueColorProcessor:
         self.combined = None
         self._red_orig_binning = 0
         self._bg_orig_binning = 0
+        self._red_orig_size = [0, 0]
 
     def process(self):
         self._size_match_bg()
@@ -177,8 +205,8 @@ class HiRISETrueColorProcessor:
             self._red_orig_binning = red_lbl["IsisCube"]["Instrument"]["Summing"]
             self._bg_orig_binning = bg_lbl["IsisCube"]["Instrument"]["Summing"]
 
-            red_samples = red_lbl["IsisCube"]["Core"]["Dimensions"]["Samples"]
-            red_lines = red_lbl["IsisCube"]["Core"]["Dimensions"]["Lines"]
+            self._red_orig_size[0] = int(red_lbl["IsisCube"]["Core"]["Dimensions"]["Samples"])
+            self._red_orig_size[1] = int(red_lbl["IsisCube"]["Core"]["Dimensions"]["Lines"])
 
             optical_distortion_correct = 1.0006
 
@@ -202,8 +230,8 @@ class HiRISETrueColorProcessor:
 
             crop = self._isis_client.command("crop")
             crop.add_arg("from", enlarged_file)
-            crop.add_arg("nsamples", red_samples)
-            crop.add_arg("nlines", red_lines)
+            crop.add_arg("nsamples", self._red_orig_size[0])
+            crop.add_arg("nlines", self._red_orig_size[1])
             crop.add_arg("to", cropped_file)
             crop.send()
 
@@ -221,23 +249,38 @@ class HiRISETrueColorProcessor:
 
     def _fix_jitter(self):
         bg_basename = strip_ext(self.bg)
-        cnet_file = "{}.cnet".format(bg_basename)
+        autoreg_file = "{}.autoreg".format(uuid4())
+        cnet_file = "{}.cnet".format(uuid4())
         slithered_file = "{}.slither.cub".format(bg_basename)
 
+        # Match the first 1/32 of the bg image to some area within the first
+        # 1/16 of the red image with at least 90% correlation. It takes a while
+        # but I couldn't get red/bg lined up using the default autoreg template
+        autoreg = self._isis_client.command("autoregtemplate")
+        autoreg.add_arg("algorithm", "MaximumCorrelation")
+        autoreg.add_arg("tolerance", 0.9)
+        autoreg.add_arg("psamp", self._red_orig_size[0] // 32)
+        autoreg.add_arg("pline", self._red_orig_size[1] // 32)
+        autoreg.add_arg("ssamp", self._red_orig_size[0] // 16)
+        autoreg.add_arg("sline", self._red_orig_size[1] // 16)
+        autoreg.add_arg("topvl", autoreg_file)
+        autoreg.send()
+
         hijitreg = self._isis_client.command("hijitreg")
-        hijitreg.add_arg("from", self.red)
-        hijitreg.add_arg("match", self.bg)
+        hijitreg.add_arg("from", self.bg)
+        hijitreg.add_arg("match", self.red)
+        hijitreg.add_arg("regdef", autoreg_file)
         hijitreg.add_arg("cnetfile", cnet_file)
         hijitreg.send()
 
         slither = self._isis_client.command("slither")
         slither.add_arg("from", self.bg)
         slither.add_arg("control", cnet_file)
-        slither.add_arg("direction", "reverse")
         slither.add_arg("to", slithered_file)
         slither.send()
 
         self._isis_client.rm(cnet_file)
+        self._isis_client.rm(autoreg_file)
         self._isis_client.rm(self.bg)
 
         self.bg = slithered_file
@@ -294,9 +337,9 @@ class HiRISETrueColorProcessor:
         )
 
         fx = self._isis_client.command("fx")
-        fx.add_arg("f1", self.bg)
-        fx.add_arg("f2", self.red)
-        fx.add_arg("equation", "[2 * f1] - [0.3 * f2]")
+        fx.add_arg("f1", self.red)
+        fx.add_arg("f2", self.bg)
+        fx.add_arg("equation", "[2 * f2] - [0.3 * f1]")
         fx.add_arg("to", self.synth_blue)
         fx.send()
 
@@ -358,8 +401,8 @@ for directory in [data_dir, output_dir]:
     if not path_exists(directory):
         makedirs(directory)
 
-image_path = "EDR/PSP/ORB_008800_008899/PSP_008831_1455"
-image_id = "PSP_008831_1455"
+image_path = "EDR/ESP/ORB_036600_036699/ESP_036618_1985"
+image_id = "ESP_036618_1985"
 detectors = ["RED4", "BG12"]
 detector_processors = list()
 
